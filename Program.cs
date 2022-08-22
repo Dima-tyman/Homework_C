fd8209f2a50fd1ef5b7c7d9ab878f0f7a1ac4605
Dima-tyman <tyman-dima@list.ru>
2022-08-22 17:21:33 +0300

    Tast 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Int32.Parse(Console.ReadLine());
+
+if (number1 == number2) {
+	Console.WriteLine("Числа равны, введите два разных числа!");
+}
+else {
+	if (number1 > number2) {
+		Console.WriteLine($"Наибольшее число: {number1}");
+		Console.WriteLine($"Наименьшее число: {number2}");
+	}
+	else {
+		Console.WriteLine($"Наибольшее число: {number2}");
+		Console.WriteLine($"Наименьшее число: {number1}");
+	}
+}
