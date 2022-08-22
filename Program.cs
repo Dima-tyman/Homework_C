947ad7d0546e869a8430b29b32d12236481486e2
Dima-tyman <tyman-dima@list.ru>
2022-08-22 17:23:21 +0300

    Tast 4

--- Program.cs
+++ Program.cs
@@ -3,17 +3,22 @@ Console.Write("Введите первое число: ");
 int number1 = Int32.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Int32.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Int32.Parse(Console.ReadLine());
 
-if (number1 == number2) {
-	Console.WriteLine("Числа равны, введите два разных числа!");
-}
-else {
-	if (number1 > number2) {
+if (number1 > number2) {
+	if (number1 > number3) {
 		Console.WriteLine($"Наибольшее число: {number1}");
-		Console.WriteLine($"Наименьшее число: {number2}");
 	}
 	else {
+		Console.WriteLine($"Наибольшее число: {number3}");
+	}
+}
+else {
+	if (number2 > number3) {
 		Console.WriteLine($"Наибольшее число: {number2}");
-		Console.WriteLine($"Наименьшее число: {number1}");
+	}
+	else {
+		Console.WriteLine($"Наибольшее число: {number3}");
 	}
 }
