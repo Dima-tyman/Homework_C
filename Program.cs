5d970fec28f67af22fd658c0fac4c2c681ece3f8
Dima-tyman <tyman-dima@list.ru>
2022-08-22 17:25:01 +0300

    Task 6

--- Program.cs
+++ Program.cs
@@ -1,24 +1,10 @@
 ﻿Console.Clear();
-Console.Write("Введите первое число: ");
-int number1 = Int32.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int number2 = Int32.Parse(Console.ReadLine());
-Console.Write("Введите третье число: ");
-int number3 = Int32.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int number = Int32.Parse(Console.ReadLine());
 
-if (number1 > number2) {
-	if (number1 > number3) {
-		Console.WriteLine($"Наибольшее число: {number1}");
-	}
-	else {
-		Console.WriteLine($"Наибольшее число: {number3}");
-	}
+if (number % 2 == 0) {
+	Console.WriteLine($"Число {number} - чётное");
 }
 else {
-	if (number2 > number3) {
-		Console.WriteLine($"Наибольшее число: {number2}");
-	}
-	else {
-		Console.WriteLine($"Наибольшее число: {number3}");
-	}
+	Console.WriteLine($"Число {number} - нечётное");
 }
