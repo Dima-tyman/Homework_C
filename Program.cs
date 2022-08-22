cbb47b55b5d9b2a19e401f3821d55eb099ac2c9e
Dima-tyman <tyman-dima@list.ru>
2022-08-22 17:52:00 +0300

    Task 8(mod)

--- Program.cs
+++ Program.cs
@@ -6,11 +6,12 @@ int i = 2;
 if (number > 1) {
 	Console.Write($"Чётные числа от 1 до {number}: ");
 	while (i <= number) {
-		//c проверкой каждого числа на чётность
+		//c проверкой каждого числа на чётность (но тут не придумал как выводить последнее число без запятой)
 		/*if (i % 2 == 0) {
 			Console.Write($"{i}, ");
 		} else {}
 		i++;*/
+		
 		Console.Write(i);
 		if (i + 2 > number) {
 			Console.Write(".");
